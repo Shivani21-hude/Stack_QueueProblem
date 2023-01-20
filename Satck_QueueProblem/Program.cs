912cb9e4e219c3394c483f364e0ab05e2fefff78
Shivani21-hude <shivani21hude@gmail.com>
2023-01-20 15:39:45 +0530

    [Shivani]-Ability to peek and Pop from the stack

---
+++ Satck_QueueProblem/Program.cs
@@ -0,0 +1,20 @@
+﻿using Satck_QueueProblem;
+using System;
+
+namespace Sack_QueueProblem
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Stack using LinkedList \n");
+            LinkedList linkedListStack = new LinkedList();
+            linkedListStack.Push(70);
+            linkedListStack.Push(30);
+            linkedListStack.Push(56);
+            linkedListStack.Display();
+           
+            Console.ReadKey();
+        }
+    }
+}
